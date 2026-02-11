0b9206a02351444f55974c6b7c3e56913880f88e
berracoso <berracosohenrique@gmail.com>
2026-02-11 00:39:24 -0300

    Fix: Reordenando inicialização do container para garantir instâncias

--- src/infrastructure/config/container.js
+++ src/infrastructure/config/container.js
@@ -1,20 +1,19 @@
 /**
- * Container de Injeção de Dependência (Versão PostgreSQL - Definitiva)
+ * Container de Injeção de Dependência (Versão PostgreSQL - Estabilizada)
  */
 
-// 1. Database
 const db = require('../database/postgres');
 
-// 2. Repositories
+// Repositories
 const UsuarioRepository = require('../repositories/PostgresUsuarioRepository');
 const EventoRepository = require('../repositories/PostgresEventoRepository');
 const ApostaRepository = require('../repositories/PostgresApostaRepository');
 
-// 3. Security
+// Security
 const BcryptHasher = require('../security/BcryptHasher');
 const SessionManager = require('../security/SessionManager');
 
-// 4. Use Cases
+// Use Cases
 const RegistrarUsuario = require('../../application/use-cases/autenticacao/RegistrarUsuario');
 const FazerLogin = require('../../application/use-cases/autenticacao/FazerLogin');
 const FazerLogout = require('../../application/use-cases/autenticacao/FazerLogout');
@@ -32,15 +31,17 @@ const AbrirFecharApostas = require('../../application/use-cases/eventos/AbrirFec
 const DefinirVencedor = require('../../application/use-cases/eventos/DefinirVencedor');
 const ResetarEvento = require('../../application/use-cases/eventos/ResetarEvento');
 
-// 5. Interface (Controllers & Middlewares)
+// Controllers
 const AuthController = require('../../interface/http/controllers/AuthController');
 const UsersController = require('../../interface/http/controllers/UsersController');
 const ApostasController = require('../../interface/http/controllers/ApostasController');
 const EventosController = require('../../interface/http/controllers/EventosController');
 
+// Middlewares
 const AuthenticationMiddleware = require('../../interface/http/middlewares/authentication');
 const AuthorizationMiddleware = require('../../interface/http/middlewares/authorization');
 
+// Routes
 const createAuthRoutes = require('../../interface/http/routes/auth.routes');
 const createUsersRoutes = require('../../interface/http/routes/users.routes');
 const createApostasRoutes = require('../../interface/http/routes/apostas.routes');
@@ -53,18 +54,21 @@ class Container {
     }
 
     _initialize() {
+        // Core
         this.instances.db = db;
-        this.instances.usuarioRepository = new UsuarioRepository(db);
-        this.instances.eventoRepository = new EventoRepository(db);
-        this.instances.apostaRepository = new ApostaRepository(db);
         this.instances.bcryptHasher = new BcryptHasher();
         this.instances.sessionManager = new SessionManager();
 
-        this._setupUseCases();
-        this._setupInterface();
-    }
+        // Repositories
+        this.instances.usuarioRepository = new UsuarioRepository(db);
+        this.instances.eventoRepository = new EventoRepository(db);
+        this.instances.apostaRepository = new ApostaRepository(db);
 
-    _setupUseCases() {
+        // Middlewares
+        this.instances.authMiddleware = new AuthenticationMiddleware(this.instances.sessionManager);
+        this.instances.authzMiddleware = new AuthorizationMiddleware(this.instances.usuarioRepository);
+
+        // Use Cases
         this.instances.registrarUsuario = new RegistrarUsuario(this.instances.usuarioRepository, this.instances.bcryptHasher);
         this.instances.fazerLogin = new FazerLogin(this.instances.usuarioRepository, this.instances.bcryptHasher, this.instances.sessionManager);
         this.instances.fazerLogout = new FazerLogout(this.instances.sessionManager);
@@ -81,19 +85,14 @@ class Container {
         this.instances.abrirFecharApostas = new AbrirFecharApostas(this.instances.eventoRepository);
         this.instances.definirVencedor = new DefinirVencedor(this.instances.eventoRepository, this.instances.apostaRepository);
         this.instances.resetarEvento = new ResetarEvento(this.instances.eventoRepository, this.instances.apostaRepository);
-    }
 
-    _setupInterface() {
-        // CORREÇÃO: Instanciando as classes normalmente como manda seu código
-        this.instances.authMiddleware = new AuthenticationMiddleware(this.instances.sessionManager);
-        this.instances.authzMiddleware = new AuthorizationMiddleware(this.instances.usuarioRepository);
-        
+        // Controllers
         this.instances.authController = new AuthController(this.instances.registrarUsuario, this.instances.fazerLogin, this.instances.fazerLogout, this.instances.obterUsuarioAtual);
         this.instances.usersController = new UsersController(this.instances.listarUsuarios, this.instances.promoverUsuario, this.instances.rebaixarUsuario);
         this.instances.apostasController = new ApostasController(this.instances.criarAposta, this.instances.listarMinhasApostas, this.instances.obterHistoricoApostas, this.instances.calcularRetornoEstimado);
         this.instances.eventosController = new EventosController(this.instances.criarNovoEvento, this.instances.obterEventoAtivo, this.instances.definirVencedor, this.instances.abrirFecharApostas, this.instances.resetarEvento);
 
-        // Injeção nas rotas
+        // Routes
         this.instances.authRoutes = createAuthRoutes(this.instances.authController, this.instances.authMiddleware);
         this.instances.usersRoutes = createUsersRoutes(this.instances.usersController, this.instances.authzMiddleware);
         this.instances.apostasRoutes = createApostasRoutes(this.instances.apostasController, this.instances.authMiddleware);
